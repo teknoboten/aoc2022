75b14396836c15f10d6a2917d89c0c417be739fe
serra <serra@boten.ca>
2022-12-04 13:27:15 -0800

    complete day 2

--- day2.js
+++ day2.js
@@ -3,8 +3,6 @@ const readline = require('readline')
 
 const rl = readline.createInterface({
   input: fs.createReadStream('day2.txt'),
-  // output: process.stdout,
-  // terminal: false
 })
 
 let totalScore = 0
@@ -18,67 +16,36 @@ const shapes = {
   Z: 3,
 }
 
-const win = ['A Y']
-const draw = ['C Z']
-const loss = ['B X']
+const win = ['A Y', 'C X', 'B Z']
+const draw = ['C Z', 'A X', 'B Y']
+const loss = ['B X', 'C Y', 'A Z']
 
-// A X
-// B X
-// C X
-// A Y
-// B Y
-// C Y
-// A Z
-// B Z
-// C Z
-
+//calculate the score for each line
 const getLineScore = (line) => {
   let lineScore = 0
 
   //value of char2
   lineScore += shapes[line[2]]
 
-  // if (line === 'A Y'){
-  //   console.log('win!')
-  //   totalScore += 6
-  // }
-
-  // if (line === 'B X'){
-  //   console.log('loss!')
-  // }
-
-  // if (line === 'C Z'){
-  //   console.log('draw!')
-  //   totalScore += 3
-  // }
-
   if (win.includes(line)) {
-    // console.log('win!')
     lineScore += 6
   }
 
   if (draw.includes(line)) {
-    // console.log('draw!')
     lineScore += 3
   }
 
   if (loss.includes(line)) {
-    // console.log('loss!')
   }
 
-  // console.log('line score', lineScore)
   return lineScore
 }
 
+//read and calculate the score for each line
+//add result to the total score
 rl.on('line', (line) => {
-  // console.log('get score:', getScore(line))
-
   let score = getLineScore(line)
   totalScore += score
-
-  // totalScore += getScore(line)
 })
 
 rl.on('close', () => console.log(totalScore))
-
-// module.exports = getTotalScore
--- getTotalScore.test.js
+++
@@ -1,5 +0,0 @@
-const getTotalScore = require('./day2')
-
-test('returns 15', () => {
-  expect(getTotalScore()).toBe(15)
-})
